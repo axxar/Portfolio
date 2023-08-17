78cc63e0d84c7eda281223ff1b12ae49b3f76928
Axxar <67249459+axxar@users.noreply.github.com>
2023-08-17 19:28:34 -0400

    git key test

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -5,7 +5,7 @@ const Portfolio = () => {
     <section className="min-h-screen flex items-center justify-center">
       <div className="text-center">
         <h1 className="text-4xl lg:text-7xl text-blue-600 font-bold mb-4">My Portfolio</h1>
-        <p className="text-lg text-gray-600">Coming Soon...!</p>
+        <p className="text-lg text-gray-600">Coming Soon...</p>
       </div>
     </section>
   );

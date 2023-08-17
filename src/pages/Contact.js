f9335bf72c2283130fc6e4c0c705a82a4c2a3edf
Axxar <akshar.athome@gmail.com>
2023-08-17 19:33:05 -0400

    Added exclamation mark

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -5,7 +5,7 @@ const Contact = () => {
     <section className="min-h-screen flex items-center justify-center">
       <div className="text-center">
         <h1 className="text-4xl lg:text-7xl text-blue-600 font-bold mb-4">Contact Me</h1>
-        <p className="text-lg text-gray-600">Coming Soon...</p>
+        <p className="text-lg text-gray-600">Coming Soon...!</p>
       </div>
     </section>
   );

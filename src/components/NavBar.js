e92279a7d32159c14f98d0093e59abc692980a54
Axxar <akshar.athome@gmail.com>
2023-08-19 18:25:24 -0400

    added flowbite, updated navbar and footer form flowbite for better ui

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,71 +1,86 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
-import APLogo from './images/AP Logo.png';
+import React from "react";
+import { Navbar } from "flowbite";
 
 const NavBar = () => {
-  const [navbar, setNavbar] = useState(false);
-
   return (
-      <nav className="fixed top-0 w-full bg-gray-950 p-4 z-10 shadow-xl">
-          <div className="container justify-between px-4 mx-auto items-center md:flex md:px-8">
-              <div>
-                  <div className="flex items-center justify-between md:block">
-                    <Link to="/" className="hidden md:block text-white text-lg font-bold">
-                        <img src={APLogo} alt="Logo" className="lg:w-14 md:w-12 sm:w-10 w-10" />
-                    </Link>
-                      <div className="md:hidden">
-                          <button
-                              className="p-2 text-gray-300 rounded-md outline-none focus:border-gray-400 focus:border"
-                              onClick={() => setNavbar(!navbar)}
-                          >
-                              {navbar ? (
-                                  <svg
-                                      xmlns="http://www.w3.org/2000/svg"
-                                      className="w-6 h-6"
-                                      viewBox="0 0 20 20"
-                                      fill="currentColor"
-                                  >
-                                      <path
-                                          fillRule="evenodd"
-                                          d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                                          clipRule="evenodd"
-                                      />
-                                  </svg>
-                              ) : (
-                                  <svg
-                                      xmlns="http://www.w3.org/2000/svg"
-                                      className="w-6 h-6"
-                                      fill="none"
-                                      viewBox="0 0 24 24"
-                                      stroke="currentColor"
-                                      strokeWidth={2}
-                                  >
-                                      <path
-                                          strokeLinecap="round"
-                                          strokeLinejoin="round"
-                                          d="M4 6h16M4 12h16M4 18h16"
-                                      />
-                                  </svg>
-                              )}
-                          </button>
-                      </div>
-                  </div>
-              </div>
-              <div>
-                  <div
-                      className={`flex-1 justify-self-center pb-3 mt-4 md:block md:pb-0 md:mt-0 md:space-x-8 ${
-                          navbar ? "block" : "hidden"
-                      }`}
-                  >
-                      <ul className="items-center justify-center space-y-2 md:flex md:space-x-6 md:space-y-0 text-gray-300 sm:text-xl text-md ">
-                        <li><Link to="/about" className="hover:text-green-400" onClick={() => setNavbar(!navbar)}>About</Link></li>
-                        <li><Link to="/portfolio" className="hover:text-green-400" onClick={() => setNavbar(!navbar)}>Portfolio</Link></li>
-                        <li><Link to="/contact" className="hover:text-green-400" onClick={() => setNavbar(!navbar)}>Contact</Link></li>
-                      </ul>
-                  </div>
-              </div>
-          </div>
-      </nav>
+    <nav class="bg-white border-gray-200 dark:bg-gray-900">
+      <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <a href="/" class="flex items-center">
+          <span class="self-center text-4xl font-bold pf-marlet-display whitespace-nowrap dark:text-blue-600 text-gray-900">
+          {"{ Akshar }"}
+          </span>
+        </a>
+        <button
+          data-collapse-toggle="navbar-default"
+          type="button"
+          class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+          aria-controls="navbar-default"
+          aria-expanded="false"
+        >
+          <span class="sr-only">Open main menu</span>
+          <svg
+            class="w-5 h-5"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 17 14"
+          >
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M1 1h15M1 7h15M1 13h15"
+            />
+          </svg>
+        </button>
+        <div class="hidden w-full md:block md:w-auto" id="navbar-default">
+          <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+            <li>
+              <a
+                href="#"
+                class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
+                aria-current="page"
+              >
+                Home
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+              >
+                About
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+              >
+                Services
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+              >
+                Pricing
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+              >
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   );
 };
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -24,7 +24,7 @@ const Home = () => {
         <div className="text-center">
           <div className="mb-5">
             <h1 className="text-4xl lg:text-7xl text-blue-600 font-bold mb-4">
-              Hi, I'm Akshar!
+              Hi, I'm <span className="pf-marlet-display font-semibold">Akshar</span>!
             </h1>
             <h1 className="text-lg lg:text-4xl text-gray-300 mb-4">
               I'm a{" "}
@@ -36,13 +36,13 @@ const Home = () => {
           <div className="mt-10">
             <Link
               to="/portfolio"
-              className="bg-blue-600 text-gray-300 px-6 py-3 rounded-full hover:bg-green-400 hover:text-gray-900 text-lg transition duration-300 mx-3 mt-10"
+              className="bg-blue-600 text-gray-300 px-3 py-2 sm:px-6 sm:py-3 rounded-full hover:bg-green-400 hover:text-gray-900 text-sm sm:text-lg transition duration-300 mx-3 mt-10"
             >
               Portfolio
             </Link>
             <Link
               to="/contact"
-              className="bg-blue-600 text-gray-300 px-6 py-3 rounded-full hover:bg-green-400 hover:text-gray-900 text-lg transition duration-300 mx-3 mt-5"
+              className="bg-blue-600 text-gray-300 px-3 py-2 sm:px-6 sm:py-3 rounded-full hover:bg-green-400 hover:text-gray-900 text-sm sm:text-lg transition duration-300 mx-3 mt-5"
             >
               Contact
             </Link>
